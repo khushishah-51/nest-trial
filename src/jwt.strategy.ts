c3c05cbf14ae60d0b0e1194c0adbe2cd9a4b95d5
khushishah-51 <khushis@zignuts.com>
2024-04-01 17:12:37 +0530

    jwt working

--- src/jwt.strategy.ts
+++ src/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secret', // Use the same secret key as in AppModule
+      secretOrKey: 'mysecret', // Use the same secret key as in AppModule
     });
   }
 
